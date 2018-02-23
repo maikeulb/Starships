d6cfb855950ee02643124d1f6c5310dc0bbd5392
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-22 21:55:56 -0500

    add starship model

---
+++ src/Starships.API/Models/Starship.cs
@@ -0,0 +1,14 @@
+using System.Collections.Generic;
+
+namespace Starship.Model
+{
+    public class Starship
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Model { get; set; }
+        public string StarshipClass { get; set; }
+        public string Manufactuerer { get; set; }
+        public string HyperdriveRating { get; set; }
+    }
+}
