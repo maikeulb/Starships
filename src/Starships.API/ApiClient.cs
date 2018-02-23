fccbf853826e491829abebe7c0033cd30050182e
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-22 21:59:32 -0500

    add api client

---
+++ src/Starships.API/ApiClient.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace Starship.API
+{
+    public static class ApiClient
+    {
+        public static async Task<T> Get<T>(string url, Func<string, T> callBack)
+        {
+            using(var client = new HttpClient())
+            {
+                using (HttpResponseMessage res = await client.GetAsync(url))
+                using (HttpContent content = res.Content)
+                {
+                    string data = await content.ReadAsStringAsync();
+                    if (data == null) 
+                    {
+                        return default(T);
+                    } else 
+                    {
+                        return callBack(data);
+                    }
+                }
+            }
+        }
+    }
+}
