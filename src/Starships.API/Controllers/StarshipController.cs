30b9382a1c9ca1c7f69954dfe384ff00e1b488f6
Michael Barnes <michael.sergio.barnes@gmail.com>
2018-02-22 22:02:19 -0500

    add starship controller

---
+++ src/Starships.API/Controllers/StarshipController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Distributed;
+using Newtonsoft.Json;
+using Starship.Model;
+using Starship.Services;
+
+namespace Starship.API.Controller
+{
+    [Route("api/[controller]")]
+    public class StarshipController : Controller
+    {
+        private readonly IStarshipService _starshipService;
+        public StarshipController(IStarshipService starshipService)
+        {
+            _starshipService = starshipService;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<Pokemon>> Get()
+        {
+            return await _starshipService.Get();
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> Get(int id)
+        {
+            var starship = await _starshipService.Get(id);
+            if (starship == null)
+            { 
+                return NotFound(id); 
+            }
+            else 
+            {
+                return Json(starship);
+            }
+        }
+    }
+}
